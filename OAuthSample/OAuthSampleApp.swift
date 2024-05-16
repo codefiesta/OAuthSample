f3e8e552242190a7779dd79c5565e94f5875c369
Kevin McKee <kevinmmckee@gmail.com>
2024-05-16 09:24:39 -0700

    Initial Commit

---
+++ OAuthSample/OAuthSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OAuthSampleApp.swift
+//  OAuthSample
+//
+//  Created by Kevin McKee on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct OAuthSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
